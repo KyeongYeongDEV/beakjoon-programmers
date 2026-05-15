3902cb44612e5105066883b7a8451c9f9678f832
최경영 <91273433+KyeongYeongDEV@users.noreply.github.com>
2026-05-15 14:18:16 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.04 ms, Memory: 4.32 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -4,31 +4,44 @@
 
 using namespace std;
 
+/*
+    1. N*N = 정사각형
+    2. 가장 아래칸부터 쌓임
+    3. 바구니로 옮김
+    4. 만약 바구니에 같은 인형이 2개 겹치면 인형 삭제
+    5. 인형이 없는 곳을 집을 시 아무일도 없음
+    6. N = 5 ~ 30
+    7. moves = 1 ~ 1000
+*/ 
+
+// 격자의 상태가 담긴 2차원 배열 board
+// 인형을 집기 위해 크레인을 작동시킨 위치가 담긴 배열 moves
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
-    stack<int> basket; 
-
-    for(int i = 0; i < moves.size(); i++) {
-        int col = moves[i] - 1;
-        int doll = 0;          
-
-        for(int j = 0; j < board.size(); j++) {
-            if (board[j][col] != 0) {
-                doll = board[j][col]; 
-                board[j][col] = 0;   
-                break; 
-            }
-        }
+   
+    int N = board.size();
+    stack<int> s;
+    
+    for(int move : moves) {
+        int cur = move - 1;
         
-        if(doll != 0) {
-            if(!basket.empty() && basket.top() == doll) {
-                basket.pop(); 
-                answer += 2;   
-            } else {
-                basket.push(doll); 
+        for(int i = 0; i < N; i++ ){
+            if(board[i][cur] != 0){
+                if( !s.empty() && s.top() == board[i][cur]) {
+                    s.pop();
+                    answer += 2;
+                } else {
+                    s.push(board[i][cur]);
+                }
+                board[i][cur] = 0;
+                
+                break;
             }
         }
     }
     
+    
+    
     return answer;
 }
+
