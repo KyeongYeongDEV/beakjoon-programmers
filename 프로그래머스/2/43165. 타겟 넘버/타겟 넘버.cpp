1703458471542a3cf18f9afab181d227de309f0c
최경영 <91273433+KyeongYeongDEV@users.noreply.github.com>
2026-05-16 00:15:54 +0900

    [level 2] Title: 타겟 넘버, Time: 2.28 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,26 +1,23 @@
-#include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 int answer = 0;
 
-void dfs(vector<int>& numbers, int target, int index, int current_sum) {
-    // 종료 조건 = 모든 숫자를 다 확인했는가
-    if (index == numbers.size()) {
-        // 지금까지의 합이 타켓과 일치하면 정답 카운트 증가
-        if(current_sum == target) {
-            answer++;
-        }
+void dfs(const vector<int>& numbers, int target, int index, int current_sum) {
+    // 종료 조건
+    // 끝까지 다 돌았을 때의 합이 target과 같다면
+    if(index == numbers.size()){
+        if(current_sum == target) answer++;
         return ;
     }
     
     dfs(numbers, target, index + 1, current_sum + numbers[index]);
-    
     dfs(numbers, target, index + 1, current_sum - numbers[index]);
 }
 
 int solution(vector<int> numbers, int target) {
-    dfs(numbers, target, 0, 0);
+    dfs(numbers, target, 0, 0);    
     return answer;
 }
