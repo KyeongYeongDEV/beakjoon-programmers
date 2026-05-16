d4661c94ae71880eb30c0f0be1bc1c1e0ddb77a0
최경영 <91273433+KyeongYeongDEV@users.noreply.github.com>
2026-05-16 13:47:44 +0900

    [level 3] Title: 네트워크, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void dfs(int cur, int n, const vector<vector<int> >& computers, vector<bool>& visited){
+    visited[cur] = true;
+    
+    for(int i =0; i < n; i++) {
+        if(computers[cur][i] == 1 && !visited[i]) {
+            dfs(i, n, computers, visited);
+        }
+    }
+} 
+
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    vector<bool> visited(n, false);
+    
+    for(int i = 0; i < n; i++) {
+        if(!visited[i]) {
+            answer++;
+            dfs(i,n,computers, visited);
+        }
+    }
+    return answer;
+}
