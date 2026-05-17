1ea55b31f6428a921bde29cc729980977f428ebf
최경영 <91273433+KyeongYeongDEV@users.noreply.github.com>
2026-05-17 15:09:04 +0900

    [level 3] Title: 네트워크, Time: 0.04 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -3,26 +3,35 @@
 
 using namespace std;
 
-void dfs(int cur, int n, const vector<vector<int> >& computers, vector<bool>& visited){
+void dfs(int cur, int n, vector<vector<int>>& computers, vector<bool>& visited){
+    // 방문하지 않은 노드 방문 처리
     visited[cur] = true;
     
-    for(int i =0; i < n; i++) {
+    // 모든 노드를 돌며
+    for(int i = 0; i < n; i++) {
+        // 현재 노드랑 연결되어 있고 방문을 하지 않았다면 
         if(computers[cur][i] == 1 && !visited[i]) {
+            // 방문 처리를 위해 재귀를 돌린다
             dfs(i, n, computers, visited);
         }
     }
-} 
-
+}
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
+    
     vector<bool> visited(n, false);
     
-    for(int i = 0; i < n; i++) {
+    // 노드를 돌면서
+    for(int i = 0; i < n ; i++) {
+        // 해당 노드에 방문을 하지 않았다면
         if(!visited[i]) {
+            // 하나의 연결된 그룹이므로 ++;
             answer++;
-            dfs(i,n,computers, visited);
+            // 해당 노드에 연결된 노드들을 방문 처리해준다.
+            dfs(i, n, computers, visited);
         }
     }
+      
     return answer;
 }
