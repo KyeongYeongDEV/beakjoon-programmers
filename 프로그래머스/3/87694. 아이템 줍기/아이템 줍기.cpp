c34baf921b60d91b4522b78419a469b77187831c
최경영 <91273433+KyeongYeongDEV@users.noreply.github.com>
2026-05-18 18:09:34 +0900

    [level 3] Title: 아이템 줍기, Time: 0.06 ms, Memory: 4.04 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -1,74 +1,74 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <algorithm>
-#include <cstring>
+
+#define MAX 101
 
 using namespace std;
 
+struct Point{
+  int x, y;  
+};
 
+int bfs(Point start, Point target, vector<vector<int>>& graph) {
+    vector<vector<int>> visited(MAX, vector<int>(MAX, 0));
+    queue<Point> q;
+    
+    int dx[] = {0,0,-1,1};
+    int dy[] = {1,-1,0,0};
+    
+    q.push(start);
+    visited[start.x][start.y] = 1;
+    
+    while(!q.empty()) {
+        int x = q.front().x;
+        int y = q.front().y;
+        q.pop();
+        
+        if(x == target.x && y == target.y) {
+            return visited[x][y] / 2;
+        }
+        
+        for(int i = 0; i < 4; i++ ) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx >= 0 && nx < MAX && ny >= 0 && ny < MAX ) {
+                if(graph[nx][ny] == 1 && !visited[nx][ny]) {
+                    q.push({nx,ny});
+                    visited[nx][ny] = visited[x][y] + 1;
+                }
+            }
+        }
+    }
+    
+    return -1;
+}
 
 int solution(vector<vector<int>> rectangle, 
              int characterX, int characterY, 
              int itemX, int itemY) {
-    // 이전 테스트 케이스의 데이터가 남지 않도록 초기화
-    int board[101][101];
-    memset(board, 0, sizeof(board));
     
-    int dx[] = {0,0,1,-1};
-    int dy[] = {1,-1,0,0};
+    Point start = {characterX * 2, characterY * 2};
+    Point target = {itemX * 2, itemY * 2};
     
-    // 모든 사각형을 맵에 그린다. 2배 확장
+    vector<vector<int>> graph(MAX, vector<int>(MAX, 0));
+    
     for(auto& rect : rectangle) {
         int x1 = rect[0] * 2, y1 = rect[1] * 2;
         int x2 = rect[2] * 2, y2 = rect[3] * 2;
         
         for(int i = x1; i <= x2; i++) {
             for(int j = y1; j <= y2; j++) {
-                
-                // 사각형의 내부일 경우 '-1'로 마킹
-                if(i > x1 && i < x2 && j > y1 && j < y2) { 
-                    board[i][j] = -1;
+                if(i > x1 && i < x2 && j > y1 && j < y2) { // 내부
+                    graph[i][j] = -1;                    
+                } else if(graph[i][j] != -1){ // 테두리
+                    graph[i][j] = 1; 
                 }
-                
-                // 사각형의 '테두리'인 경우 '1'로 마킹
-                else if(board[i][j] != -1) {
-                    board[i][j] = 1;
-                }
+
             }
         }
     }
-    
-    queue<pair<int, int> > q;
-    // 시작점 좌표 2배 확장하여 큐에 삽입
-    q.push({characterX * 2, characterY * 2});
-    
-    // 방문 여부와 거리 기록할 배열(시작점 1로 잡음)
-    vector<vector<int> > dist(101, vector<int>(101, 0));
-    dist[characterX * 2][characterY * 2] = 1;
-    
-    while(!q.empty()) {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-        
-        if(x == itemX * 2 && y == itemY * 2){
-            return (dist[x][y] - 1) / 2;
-        }
-        
-        for(int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if(nx >= 0 && nx < 101 && ny >= 0 && ny < 101){
-                // 테두리이면서 방문 전인 곳
-                if(board[nx][ny] == 1 && dist[nx][ny] == 0) { 
-                    dist[nx][ny] = dist[x][y] + 1;
-                    q.push({nx, ny});
-                }
-            }
-        }
-    }
-    
-    return 0;
+ 
+    return bfs(start, target, graph);
 }
