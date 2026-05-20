72531f600ac5c0f30069e7355d39fa56cd924def
최경영 <91273433+KyeongYeongDEV@users.noreply.github.com>
2026-05-20 16:52:58 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 3.95 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -1,22 +1,22 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-
-bool dfs(string cur, vector<vector<string>>& tickets, vector<bool>& visited, vector<string>& answer, int count){
+bool dfs(string cur, vector<vector<string>>& tickets, vector<string>& answer, 
+         vector<int>& visited, int count ){
+    
     answer.push_back(cur);
     
     if(count == tickets.size()) return true;
     
-    for (int i = 0; i < tickets.size(); i++) {
+    for( int i = 0; i < tickets.size(); i++) {
         if(tickets[i][0] == cur && !visited[i]) {
-            visited[i] = true;
+            visited[i] = 1;
             
-            if(dfs(tickets[i][1], tickets, visited, answer, count + 1)) return true;
+            if(dfs(tickets[i][1], tickets, answer, visited, count + 1)) return true;
             
-            visited[i] = false;
+            visited[i] = 0;
         }
     }
     
@@ -26,19 +26,11 @@ bool dfs(string cur, vector<vector<string>>& tickets, vector<bool>& visited, vec
 
 vector<string> solution(vector<vector<string>> tickets) {
     vector<string> answer;
-    vector<bool> visited(tickets.size(), false);
-    
+    vector<int> visited(tickets.size(), 0);
     sort(tickets.begin(), tickets.end());
     
-    dfs("ICN", tickets, visited, answer, 0);
+    dfs("ICN", tickets, answer, visited, 0);
+    
     
     return answer;
-    
 }
-/*
-    1. 항상 INC에서 출발
-    2. 3-10000
-    3. 주어진 항공권은 모두 사용해야 함
-    4. 알파벳 순으로 오름차순
-    5. 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 합니다.
-*/
